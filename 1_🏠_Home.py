c4f146360044a306f68c9aaf5c5fe495bff7442d
alexkrs <19100749-alexkrs@users.noreply.replit.com>
2023-09-15 00:57:56 +0000

    Initial commit

---
+++ 1_🏠_Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Supplements Assistant",
+    page_icon="💊",
+)
+
+st.title("Home Page")
+st.sidebar.success("Select a page above.")
---
+++ pages/3_⭐_Reviews.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Supplement Reviews",
+    page_icon="⭐",
+)
+
+st.title("⭐ Supplement Reviews")
